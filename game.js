129eb7536a314b103d16d688ddacb83533e34737
Cyber-Kira <kkoloskov02@gmail.com>
2020-10-15 11:23:28 +0300

    added more sprites

--- game.js
+++ game.js
@@ -1,14 +1,45 @@
 let game = {
-    start: function() {
+    ctx: null,
+    sprites: {
+        background: null,
+        ball: null,
+        platform: null
+    },
+    init: function() {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
-        let background = new Image();
-        background.src = "img/background.png";
+    },
+    preload(callback) {
+        let loaded = 0,
+            required = Object.keys(this.sprites).length;
+
+        for(let key in this.sprites) {
+            this.sprites[key] = new Image();
+            this.sprites[key].src = `img/sprites/${key}.png`;
+            this.sprites[key].addEventListener('load', () => {
+                loaded += 1;
+
+                if (loaded >= required) {
+                    callback();
+                }
+            });
+        }
+    },
+    render() {
+        this.ctx.drawImage(this.sprites.background, 0, 0);
+        this.ctx.drawImage(this.sprites.ball, 0, 0);
+        this.ctx.drawImage(this.sprites.platform, 0, 0);
+    },
+    run() {
         window.requestAnimationFrame(() => {
-            this.ctx.drawImage(background, 0, 0);
+            this.render();
         });
+    },
+    start: function() {
+        this.init();
+        this.preload(() => this.run());
     }
 };
 
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
     game.start();
 });
