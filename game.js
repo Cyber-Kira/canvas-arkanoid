f16a0e1da49367f50bcf55b9a403bddce1932366
Cyber-Kira <kkoloskov02@gmail.com>
2020-10-15 12:23:49 +0300

    added blocks

--- game.js
+++ game.js
@@ -1,54 +1,80 @@
 let game = {
     ctx: null,
     platform: null,
+    ball: null,
+    blocks: [],
+    rows: 4,
+    cols: 8,
     sprites: {
         background: null,
         ball: null,
-        platform: null
+        platform: null,
+        block: null
     },
-    init: function() {
+    init: function () {
         this.ctx = document.getElementById("mycanvas").getContext("2d");
     },
     preload(callback) {
         let loaded = 0,
             required = Object.keys(this.sprites).length;
 
-        for(let key in this.sprites) {
+        const onImageLoad = () => {
+            loaded += 1;
+
+            if (loaded >= required) {
+                callback();
+            }
+        };
+
+        for (let key in this.sprites) {
             this.sprites[key] = new Image();
             this.sprites[key].src = `img/sprites/${key}.png`;
-            this.sprites[key].addEventListener('load', () => {
-                loaded += 1;
-
-                if (loaded >= required) {
-                    callback();
-                }
-            });
+            this.sprites[key].addEventListener('load', onImageLoad);
         }
     },
     render() {
         this.ctx.drawImage(this.sprites.background, 0, 0);
         this.ctx.drawImage(this.sprites.ball, this.ball.x, this.ball.y);
         this.ctx.drawImage(this.sprites.platform, this.platform.x, this.platform.y);
+        this.renderBlocks();
+    },
+    create() {
+        for (let row = 0; row < this.rows; row += 1) {
+            for (let col = 0; col < this.cols; col += 1) {
+                this.blocks.push({
+                    x: 116 * col + 170,
+                    y: 50 * row + 60
+                });
+            }
+        }
+    },
+    renderBlocks() {
+        for (let block of this.blocks) {
+            this.ctx.drawImage(this.sprites.block, block.x, block.y);
+        }
     },
     run() {
         window.requestAnimationFrame(() => {
             this.render();
         });
     },
-    start: function() {
+    start: function () {
         this.init();
-        this.preload(() => this.run());
+        this.preload(() => {
+            this.create();
+            this.run();
+        });
     }
 };
 
 game.platform = {
-    x: 200,
-    y: 300
+    x: 500,
+    y: 600
 };
 
 game.ball = {
-    x: 300,
-    y: 260
+    x: 605,
+    y: 560
 };
 
 window.addEventListener("DOMContentLoaded", () => {
